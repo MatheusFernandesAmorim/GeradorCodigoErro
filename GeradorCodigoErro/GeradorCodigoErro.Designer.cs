4476128c3b42df3cc928ec2506c8a705c20fe3db
MatheusFernandesAmorim <36733901+MatheusFernandesAmorim@users.noreply.github.com>
2020-02-04 09:23:22 -0300

    Add files via upload

---
+++ GeradorCodigoErro/GeradorCodigoErro.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace GeradorCodigoErro
+{
+    partial class GeradorCodigoErro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GeradorCodigoErro));
+            this.btnGerar = new System.Windows.Forms.Button();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGerar
+            // 
+            this.btnGerar.Location = new System.Drawing.Point(29, 28);
+            this.btnGerar.Name = "btnGerar";
+            this.btnGerar.Size = new System.Drawing.Size(75, 23);
+            this.btnGerar.TabIndex = 0;
+            this.btnGerar.Text = "Gerar";
+            this.btnGerar.UseVisualStyleBackColor = true;
+            this.btnGerar.Click += new System.EventHandler(this.btnGerar_Click);
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Enabled = false;
+            this.txtCodigo.Location = new System.Drawing.Point(170, 30);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(107, 20);
+            this.txtCodigo.TabIndex = 1;
+            this.txtCodigo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // GeradorCodigoErro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(311, 84);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.btnGerar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "GeradorCodigoErro";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gerador de Código de Erro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGerar;
+        private System.Windows.Forms.TextBox txtCodigo;
+    }
+}
+
---
+++ GeradorCodigoErro/GeradorCodigoErro.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeradorCodigoErro
+{
+    public partial class GeradorCodigoErro : Form
+    {
+        public GeradorCodigoErro()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGerar_Click(object sender, EventArgs e)
+        {
+            int unidade = Convert.ToInt32(Properties.Settings.Default["Unidades"]);
+
+            int codigo = GerarNumeroCodigo(unidade);
+
+            if (codigo.ToString().Length != (unidade + 1))
+            {
+                codigo = GerarNumeroCodigo(unidade);
+            }
+
+            txtCodigo.Text = $"E{codigo.ToString()}";
+
+            Clipboard.Clear();
+            Clipboard.SetText(txtCodigo.Text);
+        }
+
+        #region ' GerarNumeroCodigo '
+        /// <summary>
+        /// Método responsável por gerar o número do código de erro
+        /// </summary>
+        /// <param name="unidade">Parâmetro que representa a quantidade de repetição dos números</param>
+        /// <returns>Um número gerado aleatóriamente, de acordo com o limite informado</returns>
+        private int GerarNumeroCodigo(int unidade)
+        {
+            Random rnd = new Random();
+
+            int valorMinimo = Convert.ToInt32($"1{GerarLimiteUnidade(0, unidade)}");
+
+            int valorMaximo = Convert.ToInt32($"9{GerarLimiteUnidade(9, unidade)}");
+
+            return rnd.Next(valorMinimo, valorMaximo);
+        } 
+        #endregion
+
+        #region ' GerarLimiteUnidade '
+        /// <summary>
+        /// Método responsável por retornar um número limite
+        /// </summary>
+        /// <param name="numero">Parâmetro que representa o número a ser repetido</param>
+        /// <param name="repeticao">Parâmetro que representa quantas vezes esse número deve ser repetido</param>
+        /// <returns>Um texto com o limite requerido</returns>
+        private string GerarLimiteUnidade(int numero, int repeticao)
+        {
+            string limite = string.Empty;
+
+            for (int i = 0; i < repeticao; i++)
+            {
+                limite += numero.ToString();
+            }
+
+            return limite;
+        }    
+        #endregion
+    }
+}
